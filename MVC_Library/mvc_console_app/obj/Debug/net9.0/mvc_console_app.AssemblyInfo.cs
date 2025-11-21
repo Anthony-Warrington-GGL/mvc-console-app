4ac7b40b57cf4810165ae5705d63370d2d894b8a
Anthony Warrington <Anthony.Warrington@gallagher.com>
2025-11-21 14:41:39 +1300

    Update random files

--- MVC_Library/mvc_console_app/obj/Debug/net9.0/mvc_console_app.AssemblyInfo.cs
+++ MVC_Library/mvc_console_app/obj/Debug/net9.0/mvc_console_app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mvc_console_app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35cb78dbe7b748531b5c6ebd11fa8f555d7db6f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("mvc_console_app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mvc_console_app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
